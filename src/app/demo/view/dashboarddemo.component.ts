cced062b07b639063739d884515bad8a2515f2ee
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-01-22 10:23:32 +0000

    TurboTable added

--- src/app/demo/view/dashboarddemo.component.ts
+++ src/app/demo/view/dashboarddemo.component.ts
@@ -13,16 +13,27 @@ export class DashboardDemoComponent implements OnInit {
 
     cars: Car[];
 
+    cols: any[];
+
     chartData: any;
 
     events: any[];
 
     selectedCity: any;
 
+    selectedCar: Car;
+
     constructor(private carService: CarService, private eventService: EventService) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => this.cars = cars);
+
+        this.cols = [
+            { field: 'vin', header: 'Vin' },
+            { field: 'year', header: 'Year' },
+            { field: 'brand', header: 'Brand' },
+            { field: 'color', header: 'Color' }
+        ];
 
         this.eventService.getEvents().then(events => {this.events = events; });
 
