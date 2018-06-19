1918719d2d49262b72510157ad5775b2dead3771
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-06-19 08:45:52 +0000

    angular v6 and prime v6

--- src/app/app.profile.component.ts
+++ src/app/app.profile.component.ts
@@ -1,7 +1,5 @@
-import {Component, Input, OnInit, EventEmitter, ViewChild, trigger, state, transition, style, animate} from '@angular/core';
-import {Location} from '@angular/common';
-import {Router} from '@angular/router';
-import {MenuItem} from 'primeng/primeng';
+import {Component} from '@angular/core';
+import {trigger, state, transition, style, animate} from '@angular/animations';
 
 @Component({
     selector: 'app-inline-profile',
