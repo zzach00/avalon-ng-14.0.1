de8b191699200a367f83dfb5d20d1626a2f5279f
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2018-06-19 19:18:41 +0000

    parentactive added

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -6,7 +6,8 @@ import {AppComponent} from './app.component';
 @Component({
     selector: 'app-menu',
     template: `
-        <ul app-submenu [item]="model" root="true" class="layout-menu layout-main-menu clearfix" [reset]="reset" visible="true"></ul>
+        <ul app-submenu [item]="model" root="true" class="layout-menu layout-main-menu clearfix"
+            [reset]="reset" visible="true" parentActive="true"></ul>
     `
 })
 export class AppMenuComponent implements OnInit {
@@ -263,7 +264,7 @@ export class AppMenuComponent implements OnInit {
                     <div class="layout-menu-tooltip-text">{{child.label}}</div>
                 </div>
                 <div class="submenu-arrow" *ngIf="child.items"></div>
-                <ul app-submenu [item]="child" *ngIf="child.items" [visible]="isActive(i)" [reset]="reset"
+                <ul app-submenu [item]="child" *ngIf="child.items" [visible]="isActive(i)" [reset]="reset" [parentActive]="isActive(i)"
                     [@children]="(app.isSlim()||app.isHorizontal())&&root ? isActive(i) ?
                      'visible' : 'hidden' : isActive(i) ? 'visibleAnimated' : 'hiddenAnimated'"></ul>
             </li>
@@ -297,6 +298,8 @@ export class AppSubMenuComponent {
     @Input() visible: boolean;
 
     _reset: boolean;
+
+    _parentActive: boolean;
 
     activeIndex: number;
 
@@ -360,6 +363,18 @@ export class AppSubMenuComponent {
         this._reset = val;
 
         if (this._reset && (this.app.isHorizontal() || this.app.isSlim())) {
+            this.activeIndex = null;
+        }
+    }
+
+    @Input() get parentActive(): boolean {
+        return this._parentActive;
+    }
+
+    set parentActive(val: boolean) {
+        this._parentActive = val;
+
+        if (!this._parentActive) {
             this.activeIndex = null;
         }
     }
