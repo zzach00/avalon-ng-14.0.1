47a0b0ff78423c991ef23a0ab3e3b6173dbd6358
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2020-09-29 09:04:06 +0300

    Update widgets.component.ts

--- src/app/utilities/widgets.component.ts
+++ src/app/utilities/widgets.component.ts
@@ -1,7 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
     templateUrl: './widgets.component.html'
 })
-export class WidgetsComponent {
+export class WidgetsComponent implements OnInit {
+
+    cities: SelectItem[];
+    
+    selectedCity: any;
+    
+    ngOnInit() {
+        this.cities = [];
+        this.cities.push({label: 'Select City', value: null});
+        this.cities.push({label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}});
+        this.cities.push({label: 'Rome', value: {id: 2, name: 'Rome', code: 'RM'}});
+        this.cities.push({label: 'London', value: {id: 3, name: 'London', code: 'LDN'}});
+        this.cities.push({label: 'Istanbul', value: {id: 4, name: 'Istanbul', code: 'IST'}});
+        this.cities.push({label: 'Paris', value: {id: 5, name: 'Paris', code: 'PRS'}});
+    }
 }
