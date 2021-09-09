cc00efcd33ecf2ba0be25e631c00a7f200f4e02a
Merve Özçifçi <merveozcifci7@gmail.com>
2021-09-09 14:20:13 +0300

    Update app.menu.component.ts

--- src/app/app.menu.component.ts
+++ src/app/app.menu.component.ts
@@ -15,7 +15,7 @@ export class AppMenuComponent implements OnInit {
     ngOnInit() {
         this.model = [
             {
-                label: 'Favorites', icon: 'pi pi-home',
+                label: 'Favorites', icon: 'pi pi-fw pi-home',
                 items: [
                     {label: 'Dashboards', icon: 'pi pi-fw pi-home', routerLink: ['/dashboards'],
                         items: [
@@ -31,7 +31,7 @@ export class AppMenuComponent implements OnInit {
                     {label: 'Form Layout', icon: 'pi pi-fw pi-id-card', routerLink: ['/uikit/formlayout']},
                     {label: 'Input', icon: 'pi pi-fw pi-check-square', routerLink: ['/uikit/input']},
                     {label: 'Float Label', icon: 'pi pi-fw pi-bookmark', routerLink: ['/uikit/floatlabel']},
-                    {label: 'Invalid State', icon: 'pi pi-exclamation-circle', routerLink: ['/uikit/invalidstate']},
+                    {label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate']},
                     {label: 'Button', icon: 'pi pi-fw pi-mobile', routerLink: ['/uikit/button'], class: 'rotated-icon'},
                     {label: 'Table', icon: 'pi pi-fw pi-table', routerLink: ['/uikit/table']},
                     {label: 'List', icon: 'pi pi-fw pi-list', routerLink: ['/uikit/list']},
@@ -120,7 +120,7 @@ export class AppMenuComponent implements OnInit {
                 ]
             },
             {
-                label: 'Start', icon: 'pi pi-download',
+                label: 'Start', icon: 'pi pi-fw pi-download',
                 items: [
                     {
                         label: 'Buy Now', icon: 'pi pi-fw pi-shopping-cart', url: ['https://www.primefaces.org/store']
