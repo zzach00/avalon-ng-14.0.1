69180276157f97efc6111492f6efd7f9158677dd
Merve Özçifçi <merveozcifci7@gmail.com>
2021-11-16 15:44:53 +0300

    chart changed

--- src/app/demo/view/dashboardbanking.component.ts
+++ src/app/demo/view/dashboardbanking.component.ts
@@ -15,6 +15,10 @@ export class DashboardBankingComponent implements OnInit {
 
     selectedDropdownItem: any;
 
+    visitorChart: any;
+
+    visitorChartOptions: any;
+
     constructor(private productService: ProductService) { }
 
     ngOnInit() {
@@ -25,5 +29,50 @@ export class DashboardBankingComponent implements OnInit {
         this.dropdownItem.push({label: 'Xbox One', value: {id: 1, name: 'Xbox One', code: 'XO'}});
         this.dropdownItem.push({label: 'PS4', value: {id: 2, name: 'PS4', code: 'PS4'}});
         this.dropdownItem.push({label: 'Wii U', value: {id: 3, name: 'Wii U', code: 'WU'}});
+
+        this.visitorChart = {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+            datasets: [
+                {
+                    data: [600, 671, 660, 665, 700, 610, 810, 790, 710, 860, 810, 780],
+                    backgroundColor: '#984082',
+                    fill: true,
+                    barPercentage: 0.75,
+                    stepped: true
+                }
+            ]
+        };
+
+        this.visitorChartOptions = {
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            responsive: true,
+            hover: {
+                mode: 'index'
+            },
+            scales: {
+                y: {
+                    min: 500,
+                    max: 900,
+                    ticks: {
+                        color: '#A0A7B5'
+                    },
+                    grid: {
+                        color:  'rgba(160, 167, 181, .3)',
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: '#A0A7B5'
+                    },
+                    grid: {
+                        display: false
+                    }
+                }
+            }
+        };
     }
 }
